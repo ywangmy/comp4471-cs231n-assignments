7885f6c198a105ad351b77e9ce65be3df0b60d08
yc wang <ywangmy@connect.ust.hk>
2022-11-24 20:10:46 +0800

    update

---
+++ assignment3/LSTM_Captioning.py
@@ -0,0 +1,115 @@
+# As usual, a bit of setup
+from __future__ import print_function
+import time, os, json
+import numpy as np
+import matplotlib.pyplot as plt
+
+from cs231n.gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
+from cs231n.rnn_layers import *
+from cs231n.captioning_solver import CaptioningSolver
+from cs231n.classifiers.rnn import CaptioningRNN
+from cs231n.coco_utils import load_coco_data, sample_coco_minibatch, decode_captions
+from cs231n.image_utils import image_from_url
+import nltk
+
+plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+def rel_error(x, y):
+    """ returns relative error """
+    return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
+
+################################################################################
+
+# Load COCO data from disk; this returns a dictionary
+# We'll work with dimensionality-reduced features for this notebook, but feel
+# free to experiment with the original features by changing the flag below.
+data = load_coco_data(pca_features=True)
+
+# Print out all the keys and values from the data dictionary
+for k, v in data.items():
+    if type(v) == np.ndarray:
+        print(k, type(v), v.shape, v.dtype)
+    else:
+        print(k, type(v), len(v))
+
+################################################################################
+
+def BLEU_score(gt_caption, sample_caption):
+    """
+    gt_caption: string, ground-truth caption
+    sample_caption: string, your model's predicted caption
+    Returns unigram BLEU score.
+    """
+    reference = [x for x in gt_caption.split(' ') 
+                 if ('<END>' not in x and '<START>' not in x and '<UNK>' not in x)]
+    hypothesis = [x for x in sample_caption.split(' ') 
+                  if ('<END>' not in x and '<START>' not in x and '<UNK>' not in x)]
+    BLEUscore = nltk.translate.bleu_score.sentence_bleu([reference], hypothesis, weights = [1])
+    return BLEUscore
+
+def evaluate_model(model):
+    """
+    model: CaptioningRNN model
+    Prints unigram BLEU score averaged over 1000 training and val examples.
+    """
+    BLEUscores = {}
+    for split in ['train', 'val']:
+        minibatch = sample_coco_minibatch(med_data, split=split, batch_size=1000)
+        gt_captions, features, urls = minibatch
+        gt_captions = decode_captions(gt_captions, data['idx_to_word'])
+
+        sample_captions = model.sample(features)
+        sample_captions = decode_captions(sample_captions, data['idx_to_word'])
+
+        total_score = 0.0
+        for gt_caption, sample_caption, url in zip(gt_captions, sample_captions, urls):
+            total_score += BLEU_score(gt_caption, sample_caption)
+
+        BLEUscores[split] = total_score / len(sample_captions)
+
+    for split in BLEUscores:
+        print('Average BLEU score for %s: %f' % (split, BLEUscores[split]))
+
+################################################################################
+
+# med_data = load_coco_data(max_train=50000)
+med_data = data
+
+np.random.seed(231)
+
+big_data = load_coco_data(max_train=20)
+
+big_lstm_model = CaptioningRNN(
+    cell_type='lstm',
+    word_to_idx=data['word_to_idx'],
+    input_dim=data['train_features'].shape[1],
+    hidden_dim=512,
+    wordvec_dim=256,
+    dtype=np.float32,
+)
+
+big_lstm_solver = CaptioningSolver(
+    big_lstm_model, big_data,
+    update_rule='adam',
+    num_epochs=50,
+    batch_size=25,
+    optim_config={
+        'learning_rate': 5e-4,
+    },
+    lr_decay=0.995,
+    verbose=True, print_every=100,
+)
+    
+big_lstm_solver.train()
+
+# Plot the training losses
+plt.plot(big_lstm_solver.loss_history)
+plt.xlabel('Iteration')
+plt.ylabel('Loss')
+plt.title('Training loss history')
+#plt.show()
+plt.savefig('books_read.png')
+
+evaluate_model(big_lstm_model)
