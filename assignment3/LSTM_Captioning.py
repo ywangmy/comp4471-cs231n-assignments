4dc4bcd4ba5d010a637f224463ff7ffde7ac70e7
yc wang <ywangmy@connect.ust.hk>
2022-11-25 13:27:43 +0800

    py

--- assignment3/LSTM_Captioning.py
+++ assignment3/LSTM_Captioning.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 import time, os, json
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+import pickle
 
 from cs231n.gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
 from cs231n.rnn_layers import *
@@ -12,20 +14,19 @@ from cs231n.coco_utils import load_coco_data, sample_coco_minibatch, decode_capt
 from cs231n.image_utils import image_from_url
 import nltk
 
-plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
+# Load COCO data from disk; this returns a dictionary
+# We'll work with dimensionality-reduced features for this notebook, but feel
+# free to experiment with the original features by changing the flag below.
+data = load_coco_data(pca_features=True)
+# med_data = load_coco_data(max_train=50000)
+med_data = data
+big_lstm_model = None
 
 def rel_error(x, y):
     """ returns relative error """
     return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
 
 ################################################################################
-
-# Load COCO data from disk; this returns a dictionary
-# We'll work with dimensionality-reduced features for this notebook, but feel
-# free to experiment with the original features by changing the flag below.
-data = load_coco_data(pca_features=True)
 
 # Print out all the keys and values from the data dictionary
 for k, v in data.items():
@@ -74,43 +75,66 @@ def evaluate_model(model):
 
 ################################################################################
 
-# med_data = load_coco_data(max_train=50000)
-med_data = data
+def main():
+    args = sys.argv[1:]
 
-np.random.seed(231)
+    plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
+    plt.rcParams['image.interpolation'] = 'nearest'
+    plt.rcParams['image.cmap'] = 'gray'
 
-big_data = load_coco_data(max_train=8000)
-#big_data = data
-
-big_lstm_model = CaptioningRNN(
-    cell_type='lstm',
-    word_to_idx=data['word_to_idx'],
-    input_dim=data['train_features'].shape[1],
-    hidden_dim=1024,
-    wordvec_dim=512,
-    dtype=np.float32,
-)
-
-big_lstm_solver = CaptioningSolver(
-    big_lstm_model, big_data,
-    update_rule='adam',
-    num_epochs=50,
-    batch_size=128,
-    optim_config={
-        'learning_rate': 5e-4,
-    },
-    lr_decay=0.9965,
-    verbose=True, print_every=10,
-)
+    np.random.seed(231)
     
-big_lstm_solver.train()
+    big_data = load_coco_data(max_train=8000)
+    #big_data = data
 
-# Plot the training losses
-plt.plot(big_lstm_solver.loss_history)
-plt.xlabel('Iteration')
-plt.ylabel('Loss')
-plt.title('Training loss history')
-#plt.show()
-plt.savefig('result.png')
+    if (len(args) > 0 and args[0] == '--read'):
+        with open('old_model.pkl', 'rb') as handle:
+            big_lstm_model = pickle.load(handle)
+    else:
+        big_lstm_model = CaptioningRNN(
+            cell_type='lstm',
+            word_to_idx=data['word_to_idx'],
+            input_dim=data['train_features'].shape[1],
+            hidden_dim=1024,
+            wordvec_dim=512,
+            dtype=np.float32,
+        )
 
-evaluate_model(big_lstm_model)
+    if (len(args) > 1 and args[1] == '--eval'):
+        evaluate_model(big_lstm_model)
+        exit()
+
+    big_lstm_solver = CaptioningSolver(
+    big_lstm_model, big_data,
+        update_rule='adam',
+        num_epochs=50,
+        batch_size=128,
+        optim_config={
+        'learning_rate': 5e-4,
+        },
+        lr_decay=0.9965,
+        verbose=True, print_every=10,
+    )
+
+    big_lstm_solver.train()
+    with open('new_model.pkl', 'wb') as handle:
+        pickle.dump(big_lstm_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+    # Plot the training losses
+    plt.plot(big_lstm_solver.loss_history)
+    plt.xlabel('Iteration')
+    plt.ylabel('Loss')
+    plt.title('Training loss history')
+    #plt.show()
+    plt.savefig('result.png')
+
+    evaluate_model(big_lstm_model)
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        with open('tmp_model.pkl', 'wb') as handle:
+            pickle.dump(big_lstm_model, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        sys.exit(0)
